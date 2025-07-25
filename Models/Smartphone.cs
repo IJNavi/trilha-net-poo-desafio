4b96256a64d77e4d7fe302a005c8f5eee66c74e0
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-07-25 05:13:55 -0300

    feat: Partes faltantes dos códigos de cada arquivo implementadas. Simulação de Smartphones para treino de conceitos de herança de classe abstrata concluída. Desafio concluído!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,19 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -24,3 +29,36 @@ namespace DesafioPOO.Models
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
+
+
+
+
+
+
+
+// namespace DesafioPOO.Models
+// {
+//     public abstract class Smartphone
+//     {
+//         public string Numero { get; set; }
+//         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+//         public Smartphone(string numero)
+//         {
+//             Numero = numero;
+//             // TODO: Passar os parâmetros do construtor para as propriedades
+//         }
+
+//         public void Ligar()
+//         {
+//             Console.WriteLine("Ligando...");
+//         }
+
+//         public void ReceberLigacao()
+//         {
+//             Console.WriteLine("Recebendo ligação...");
+//         }
+
+//         public abstract void InstalarAplicativo(string nomeApp);
+//     }
+// }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Iphone iphone = new Iphone(numero: "654321", modelo: "iPhone 15", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
+
+//using DesafioPOO.Models;
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
